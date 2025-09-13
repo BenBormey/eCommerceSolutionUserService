6c432570115a74da5c3a4216620689c08982f9b9
BenBormey <boemry902@gmail.com>
2025-09-13 13:43:09 +0700

    9/13

--- eCommerce.API/Controllers/BookingController.cs
+++ eCommerce.API/Controllers/BookingController.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerce.Core.DTO.Booking;
 using eCommerce.Core.ServiceContracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +8,7 @@ namespace eCommerce.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class BookingController : ControllerBase
     {
         private readonly IBookingService _service;
